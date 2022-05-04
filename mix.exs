f46f5f96be90d6ce5c32682bbdeeb75a94ef120c
Erick Navarro <ericknavarro@resuelve.mx>
2022-05-04 18:03:18 -0500

    Bump version 0.2.1

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Bali.MixProject do
   def project do
     [
       app: :bali,
-      version: "0.2.0",
+      version: "0.2.1",
       description: "Validate personal and tax identifiers for mx, co, es, pt, it, br",
       elixir: "~> 1.9",
       start_permanent: Mix.env() == :prod,
